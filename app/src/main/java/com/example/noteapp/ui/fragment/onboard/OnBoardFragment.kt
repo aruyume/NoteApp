8ebc78e533355ac261f2e7e68e42b97bbd2d5c88
aruyume <aruukerahmanovaa@gmail.com>
2024-05-10 23:17:03 +0600

    On Board, Design, Text

---
+++ app/src/main/java/com/example/noteapp/ui/fragment/onboard/OnBoardFragment.kt
@@ -0,0 +1,53 @@
+package com.example.noteapp.ui.fragment.onboard
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.viewpager2.widget.ViewPager2
+import com.example.noteapp.R
+import com.example.noteapp.databinding.FragmentOnBoardBinding
+import com.example.noteapp.ui.adapter.OnBoardViewPagerAdapter
+
+class OnBoardFragment : Fragment() {
+
+    private lateinit var binding: FragmentOnBoardBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentOnBoardBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initialize()
+        setupListener()
+    }
+
+    private fun initialize() {
+        binding.viewPager.adapter = OnBoardViewPagerAdapter(this)
+    }
+
+    private fun setupListener() = with(binding.viewPager) {
+        binding.viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                if (position == 2) {
+                    binding.tvSkip.visibility = View.INVISIBLE
+                } else {
+                    binding.tvSkip.visibility = View.VISIBLE
+                }
+            }
+        })
+        binding.tvSkip.setOnClickListener {
+            if (currentItem < 3) {
+                setCurrentItem(currentItem + 2, true)
+            }
+        }
+    }
+}
