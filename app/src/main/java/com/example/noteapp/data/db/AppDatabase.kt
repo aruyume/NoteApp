86069e45b691a8636e3f76ff07bd5499377a5c5f
aruyume <aruukerahmanovaa@gmail.com>
2024-05-30 14:42:40 +0600

    Drawer Navigation, Chat, Firestore

--- app/src/main/java/com/example/noteapp/data/db/AppDatabase.kt
+++ app/src/main/java/com/example/noteapp/data/db/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import com.example.noteapp.data.db.daos.NoteDao
 import com.example.noteapp.data.model.NoteModel
 
-@Database(entities = [NoteModel::class], version = 8)
+@Database(entities = [NoteModel::class], version = 10)
 abstract class AppDatabase: RoomDatabase() {
 
     abstract fun noteDao(): NoteDao
--- app/src/main/java/com/example/noteapp/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/noteapp/ui/activity/MainActivity.kt
@@ -2,28 +2,85 @@ package com.example.noteapp.ui.activity
 
 import PreferenceHelper
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import com.example.noteapp.R
+import androidx.appcompat.widget.Toolbar
 import com.example.noteapp.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding:ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var preferenceHelper: PreferenceHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        var preferenceHelper = PreferenceHelper(this)
+        // Установка ActionBar
+        setSupportActionBar(binding.toolbar)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(androidx.navigation.fragment.R.id.nav_host_fragment_container) as NavHostFragment
-        val navController = navHostFragment.navController
+        // Инициализация DrawerLayout и NavController
+        drawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        navController = navHostFragment.navController
 
+        // Настройка AppBarConfiguration
+        appBarConfiguration = AppBarConfiguration(setOf(R.id.noteFragment, R.id.chatFragment), drawerLayout)
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+        NavigationUI.setupWithNavController(navView, navController)
+
+        // Настройка Drawer Toggle
+        actionBarDrawerToggle = ActionBarDrawerToggle(this, drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        // Инициализация PreferenceHelper
+        preferenceHelper = PreferenceHelper(this)
+
+        // Проверка состояния PreferenceHelper и навигация
         if (preferenceHelper.isOnBoardShown) {
             navController.navigate(R.id.noteFragment)
         } else {
             navController.navigate(R.id.onBoardFragment)
         }
+
+        // Обработка нажатия на пункт меню Chat
+        navView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_chat -> {
+                    navController.navigate(R.id.chatFragment)
+                    drawerLayout.closeDrawers() // Закрываем NavigationDrawer
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            true
+        } else super.onOptionsItemSelected(item)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
+
