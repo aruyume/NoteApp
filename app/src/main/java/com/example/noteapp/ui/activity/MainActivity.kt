935e05ec6a0c61218f704f967101dbb39671a428
aruyume <aruukerahmanovaa@gmail.com>
2024-06-03 15:19:29 +0600

    Push Notification

--- app/src/main/java/com/example/noteapp/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/noteapp/ui/activity/MainActivity.kt
@@ -58,11 +58,13 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
         navController = navHostFragment.navController
 
+        //AppBarConfiguration
         appBarConfiguration =
             AppBarConfiguration(setOf(R.id.noteFragment, R.id.chatFragment), drawerLayout)
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
         NavigationUI.setupWithNavController(navView, navController)
 
+        //Drawer Toggle
         actionBarDrawerToggle = ActionBarDrawerToggle(
             this,
             drawerLayout,
