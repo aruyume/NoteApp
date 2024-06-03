3809153db22fc7cd87b957d3483a7526bbe72a15
aruyume <aruukerahmanovaa@gmail.com>
2024-06-03 15:13:36 +0600

    Push Notification, Permission

--- app/src/main/java/com/example/noteapp/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/noteapp/ui/activity/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.example.noteapp.ui.activity
 
 import PreferenceHelper
+import android.Manifest
+import android.app.AlertDialog
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
@@ -12,6 +18,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.example.noteapp.R
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import com.example.noteapp.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationView
 
@@ -24,25 +31,44 @@ class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var preferenceHelper: PreferenceHelper
 
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            Toast.makeText(
+                this, "Разрешение на отправку уведомлений предоставлено", Toast.LENGTH_SHORT
+            ).show()
+        } else {
+            Toast.makeText(
+                this, "Разрешение на отправку уведомлений не предоставлено", Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        
+
         setSupportActionBar(binding.toolbar)
 
         drawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
         navController = navHostFragment.navController
 
-        //AppBarConfiguration
-        appBarConfiguration = AppBarConfiguration(setOf(R.id.noteFragment, R.id.chatFragment), drawerLayout)
+        appBarConfiguration =
+            AppBarConfiguration(setOf(R.id.noteFragment, R.id.chatFragment), drawerLayout)
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
         NavigationUI.setupWithNavController(navView, navController)
 
-        //Drawer Toggle
-        actionBarDrawerToggle = ActionBarDrawerToggle(this, drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        actionBarDrawerToggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
         drawerLayout.addDrawerListener(actionBarDrawerToggle)
         actionBarDrawerToggle.syncState()
 
@@ -63,9 +89,52 @@ class MainActivity : AppCompatActivity() {
                     drawerLayout.closeDrawers()
                     true
                 }
+
                 else -> false
             }
         }
+        askNotificationPermission()
+    }
+
+    private fun askNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            when {
+                ContextCompat.checkSelfPermission(
+                    this, Manifest.permission.POST_NOTIFICATIONS
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    Toast.makeText(
+                        this,
+                        "Разрешение на отправку уведомлений уже предоставлено",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS) -> {
+                    showPermissionAlertDialog()
+                }
+
+                else -> {
+                    requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+                }
+            }
+        }
+    }
+
+    private fun showPermissionAlertDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("Необходим доступ к уведомлениям")
+            .setMessage("Для работы приложения требуется разрешение на отправку уведомлений")
+            .setPositiveButton("ОК") { _, _ ->
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
+            .setNegativeButton("Нет") { dialog, _ ->
+                // Разрешение не предоставлено
+                dialog.dismiss()
+                Toast.makeText(
+                    this, "Вы отказались от отправок уведомлений", Toast.LENGTH_SHORT).show()
+            }
+            .create()
+            .show()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -75,7 +144,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return NavigationUI.navigateUp(navController, appBarConfiguration) || super.onSupportNavigateUp()
+        return NavigationUI.navigateUp(
+            navController,
+            appBarConfiguration
+        ) || super.onSupportNavigateUp()
     }
 }
 
